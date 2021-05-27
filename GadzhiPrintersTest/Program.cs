70687438d8b51d3f0fe50e27868118ff958b4395
Ivan Rubilnik <rubilnik4@yandex.ru>
2021-05-27 18:22:24 +0500

    Printers Config
    Printers Config

---
+++ GadzhiPrintersTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadzhiPrintersTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printers = PrinterSettings.InstalledPrinters.Cast<string>();
+            foreach (var printer in printers)
+            {
+                Console.WriteLine(printer);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
